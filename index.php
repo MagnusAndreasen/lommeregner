f5a0a09d7c244ad88724a8950360a42143d7a561
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-08-23 12:05:55 +0200

    added division symbol
    added division symbol to the calculator

--- index.php
+++ index.php
@@ -11,39 +11,47 @@
 	<form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
 		<input type="number" name="val1" required/>
 		<br><br>
+		<input type="number" name="val2" required />
+		<br><br>
 		<button type="submit" name="operator" value="add">+</button>
 		<button type="submit" name="operator" value="sub">-</button>
 		<button type="submit" name="operator" value="mul">x</button>
-		<button type="submit" name="operator" value="div">/</button>
+		<button type="submit" name="operator" value="div">÷</button>
 		<br><br>
-		<input type="number" name="val2" required />
+		
+		
 	</form>
 	
 	<?php 
+		$op = $_GET["operator"];
 		// $v1 = $_GET["val1"];
 		// $v2 = $_GET["val2"];
 		$v1 = filter_input(INPUT_GET, "val1", FILTER_VALIDATE_INT) or die("missing or illegal val1 parameter");
 		$v2 = filter_input(INPUT_GET, "val2", FILTER_VALIDATE_INT) or die("missing or illegal val2 parameter");  
-		$op = $_GET["operator"];
+		
 		  
 		switch($op){
 			case "add":
 				$res = $v1+$v2;
+				$opchar = "+";
 				break;
 			case "sub":
 				$res = $v1-$v2;
+				$opchar = "-";
 				break;
 			case "mul":
 				$res = $v1*$v2;
+				$opchar = "x";
 				break;
 			case "div":
 				$res = $v1/$v2;
+				$opchar = "÷";
 				break;
 			default: 
 				$res = "Unknown operator".$op."";
 		}
 
-		echo $v1." ".$op." ".$v2." = ".$res;
+		echo $v1." ".$opchar." ".$v2." = ".$res;
 	?>
 </body>
 </html>
