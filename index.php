8fc6ec25c199af4adae9d79ffd1c48a7288db4b3
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-08-23 11:37:33 +0200

    added switch and cases

--- index.php
+++ index.php
@@ -8,6 +8,40 @@
 <body>
 	<h1>King of calculators</h1>
 	
-	<form action="<?=$_SERVER["PHP_SELF"]"?> method="get"
+	<form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
+		<input type="text" name="val1"/>
+		<br><br>
+		<button type="submit" name="operator" value="add">+</button>
+		<button type="submit" name="operator" value="sub">-</button>
+		<button type="submit" name="operator" value="mul">*</button>
+		<button type="submit" name="operator" value="div">/</button>
+		<br><br>
+		<input type="text" name="val2"/>
+	</form>
+	
+	<?php 
+		$v1 = $_GET["val1"];
+		$v2 = $_GET["val2"];
+		$op = $_GET["operator"];
+		  
+		switch($op){
+			case "add":
+				$res = $v1+$v2;
+				break;
+			case "sub":
+				$res = $v1-$v2;
+				break;
+			case "mul":
+				$res = $v1*$v2;
+				break;
+			case "div":
+				$res = $v1/$v2;
+				break;
+			default: 
+				$res = "Unknown operator".$op."";
+		}
+
+		echo $v1." ".$op." ".$v2." = ".$res;
+	?>
 </body>
 </html>
