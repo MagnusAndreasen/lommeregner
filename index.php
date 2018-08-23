000d851d8ee1a717b00d2ba11699aee70d62979f
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-08-23 11:16:57 +0200

    added form

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Lommeregner</title>
+</head>
+
+<body>
+	<h1>King of calculators</h1>
+	
+	<form action="<?=$_SERVER["PHP_SELF"]"?> method="get"
+</body>
+</html>
