3e6e0eeda856c65c2263882298901ee6bd77cea4
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-08-24 13:54:43 +0200

    added clear function and styling
    added a clear button and styling to the page and calculator

--- index.php
+++ index.php
@@ -3,33 +3,32 @@
 <head>
 <meta charset="utf-8">
 <title>Lommeregner</title>
+<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 
 <body>
+<div class="calculator">
 	<h1>King of calculators</h1>
-	
-	<form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
-		<input type="number" name="val1" required/>
-		<br><br>
-		<input type="number" name="val2" required />
-		<br><br>
-		<button type="submit" name="operator" value="add">+</button>
-		<button type="submit" name="operator" value="sub">-</button>
-		<button type="submit" name="operator" value="mul">x</button>
-		<button type="submit" name="operator" value="div">÷</button>
-		<br><br>
-		
-		
-	</form>
-	
+		<form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
+			<input class="input" type="number" name="val1" value="<?=$_GET["val1"]??""?>" required placeholder="Indtast et tal" autocomplete="off"/>
+			<br><br>
+			<input type="number" name="val2" value="<?=$_GET["val2"]??""?>" required placeholder="Indtast et tal" autocomplete="off"/>
+			<br><br>
+			<button type="submit" name="operator" value="add">+</button>
+			<button type="submit" name="operator" value="sub">-</button>
+			<button type="submit" name="operator" value="mul">x</button>
+			<button type="submit" name="operator" value="div">÷</button>
+			<button type="submit" name="operator" value="mod">%</button>
+			<br><br>
+		</form>
 	<?php 
 		$op = $_GET["operator"];
+		if (!empty($op) ){
 		// $v1 = $_GET["val1"];
 		// $v2 = $_GET["val2"];
 		$v1 = filter_input(INPUT_GET, "val1", FILTER_VALIDATE_INT) or die("missing or illegal val1 parameter");
 		$v2 = filter_input(INPUT_GET, "val2", FILTER_VALIDATE_INT) or die("missing or illegal val2 parameter");  
 		
-		  
 		switch($op){
 			case "add":
 				$res = $v1+$v2;
@@ -47,11 +46,19 @@
 				$res = $v1/$v2;
 				$opchar = "÷";
 				break;
+			case "mod":
+				$res = $v1%$v2;
+				$opchar = "%";
+				break;
 			default: 
 				$res = "Unknown operator".$op."";
 		}
-
 		echo $v1." ".$opchar." ".$v2." = ".$res;
+		}
 	?>
+	<form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
+		<button type="submit">Clear</button>
+	</form>
+</div>
 </body>
 </html>
