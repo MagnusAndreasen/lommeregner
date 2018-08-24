7270d62c0e21173d85a8374e4b36a54c72ffc460
MagnusAndreasen <31634063+MagnusAndreasen@users.noreply.github.com>
2018-08-24 14:39:51 +0200

    added styling to echo
    Added styling to result echo. But without a satisfying result.

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 
 <body>
 <div class="calculator">
-	<h1>King of calculators</h1>
+	<h1>King of Calculators</h1>
 		<form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
-			<input class="input" type="number" name="val1" value="<?=$_GET["val1"]??""?>" required placeholder="Indtast et tal" autocomplete="off"/>
+			<input class="input1" type="number" name="val1" value="<?=$_GET["val1"]??""?>" required placeholder="Indtast et tal" autocomplete="off"/>
 			<br><br>
-			<input type="number" name="val2" value="<?=$_GET["val2"]??""?>" required placeholder="Indtast et tal" autocomplete="off"/>
+			<input class="input2" type="number" name="val2" value="<?=$_GET["val2"]??""?>" required placeholder="Indtast et tal" autocomplete="off"/>
 			<br><br>
 			<button type="submit" name="operator" value="add">+</button>
 			<button type="submit" name="operator" value="sub">-</button>
@@ -26,8 +26,10 @@
 		if (!empty($op) ){
 		// $v1 = $_GET["val1"];
 		// $v2 = $_GET["val2"];
-		$v1 = filter_input(INPUT_GET, "val1", FILTER_VALIDATE_INT) or die("missing or illegal val1 parameter");
-		$v2 = filter_input(INPUT_GET, "val2", FILTER_VALIDATE_INT) or die("missing or illegal val2 parameter");  
+		$v1 = filter_input(INPUT_GET, "val1", FILTER_VALIDATE_INT);
+		if ($v1 === false) {die("missing or illegal val1 parameter");}
+		$v2 = filter_input(INPUT_GET, "val2", FILTER_VALIDATE_INT);
+		if ($v2 === false) {die("missing or illegal val2 parameter");}
 		
 		switch($op){
 			case "add":
@@ -53,9 +55,14 @@
 			default: 
 				$res = "Unknown operator".$op."";
 		}
-		echo $v1." ".$opchar." ".$v2." = ".$res;
+		
 		}
 	?>
+	<form>
+  		<output class="output"><?php echo $v1." ".$opchar." ".$v2." = ".$res; ?></output>
+	</form>
+	<!--<div class="output"><?php echo $v1." ".$opchar." ".$v2." = ".$res; ?></div> -->
+	<br><br>
 	<form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
 		<button type="submit">Clear</button>
 	</form>
